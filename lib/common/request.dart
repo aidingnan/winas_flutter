b36626a7556ff7221348ab5ddd8a08b48ec7b5ff
lixinwei <xinwei.li@winsuntech.cn>
2019-05-15 09:33:55 +0800

    change cloudAddress

--- lib/common/request.dart
+++ lib/common/request.dart
@@ -4,7 +4,7 @@ import 'package:dio/dio.dart';
 
 class Request {
   bool isIOS = !Platform.isAndroid;
-  final cloudAddress = 'http://test.aidingnan.com:12345/c/v1';
+  final cloudAddress = 'http://abel-lb-test-2102038915.cn-north-1.elb.amazonaws.com.cn:12345/c/v1';
   // final cloudAddress = 'https://test.nodetribe.com/c/v1';
   String token;
   String cookie;
